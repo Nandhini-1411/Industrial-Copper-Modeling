e06e2f153bf82f6606231d60b09bf9ffd9619f73
NandhiniVaradharajan <nandhinivaradharajan14@gmail.com>
2025-03-16 12:18:53 +0530

    Update Copper_Modeling.py

--- Copper_Modeling.py
+++ Copper_Modeling.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 import os
+import gdown
 from streamlit_option_menu import option_menu
 item_type_encoded = {'IPL': 0, 'Others': 1, 'PL': 2, 'S': 3, 'SLAWR': 4, 'W': 5, 'WI': 6}
 status_encoded = {'Draft': 0,'Lost': 1,'Not lost for AM': 2,'Offerable': 3,'Offered': 4,'Revised': 5,
@@ -13,10 +14,13 @@ def predict_status(country,item_type,order_day,order_month,order_year,delivery_y
     order_day = item_date_input.day
     order_month = item_date_input.month
     order_year = item_date_input.year
-    if not os.path.exists("Classification_Model.pkl"):
-      os.system("git lfs pull")
-    with open("Model_Files/Classification_Model.pkl", "rb") as f:
-        model_class = pickle.load(f)
+    url = "https://drive.google.com/file/d/15hTMPNsvtHry2lwwSbX9ytRExKJIoC_E/view?usp=drive_link"
+    output = "Classification_Model.pkl"
+    if not os.path.exists(output):
+        gdown.download(url, output, quiet=False)
+    # Load the model
+    with open(output, "rb") as f:
+        model = pickle.load(f)
     user_data = np.array([[country,item_type_encoded_value, order_year, order_month, order_day, delivery_year, 
                             delivery_month, delivery_day, application, width, product_ref, thickness_boxcox, 
                             quantity_tons_boxcox, selling_price]])
@@ -30,8 +34,14 @@ def predict_selling_price(country, status, item_type, item_date_input, delivery_
     order_day = item_date_input.day
     order_month = item_date_input.month
     order_year = item_date_input.year
-    with open("Model_Files/Regression_Model.pkl", "rb") as f:
-        model_regg = pickle.load(f)
+    url = "https://drive.google.com/file/d/1mRZeAqbhJ6twbbpYSxubPQd-_illKukq/view?usp=drive_link"
+    output = "Regression_Model.pkl"
+    if not os.path.exists(output):
+        gdown.download(url, output, quiet=False)
+    # Load the model
+    with open(output, "rb") as f:
+        model = pickle.load(f)
+
     user_data = np.array([[country, status_encoded_value, item_type_encoded_value, order_year, order_month,
                             order_day, delivery_year, delivery_month, delivery_day, application, width,
                               product_ref, thickness_boxcox, quantity_tons_boxcox]])
